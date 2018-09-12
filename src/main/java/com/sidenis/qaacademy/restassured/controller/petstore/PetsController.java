dcb419354c3611721e52083daf605c60089c4fb1
Ksenia.Orlova <orlova7115@gmail.com>
2018-09-12 11:23:03 +0700

    added findByStatusTest

--- src/main/java/com/sidenis/qaacademy/restassured/controller/petstore/PetsController.java
+++ src/main/java/com/sidenis/qaacademy/restassured/controller/petstore/PetsController.java
@@ -3,6 +3,8 @@ package com.sidenis.qaacademy.restassured.controller.petstore;
 import com.sidenis.qaacademy.restassured.vo.petstore.Pet;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,5 +18,37 @@ public final class PetsController {
 
     public static Pet getPet(Long id) {
         return rs.get("/" + id + "/").then().log().all().extract().as(Pet.class);
+    }
+
+    public static Pet postRequestTest(String pet){
+        return rs.body(pet).post().then().log().all().extract().as(Pet.class);
+    }
+
+    public static Pet updatePet(String pet){
+        //return rs.body(pet).put("name", "Bob").then().log().all().extract().as(Pet.class);
+        return rs.body(pet).put().then().log().all().extract().as(Pet.class);
+    }
+
+    public static Pet[] findStatus(boolean isAvailable, boolean pending, boolean sold){
+
+        //return rs.get("findByStatus?status=sold").then().log().all().extract().as(Pet[].class);
+        String status = "findByStatus?status=";
+        if (isAvailable){
+            status += "available";
+            if (pending){
+                status += "&status=pending";
+                if (sold){
+                    status += "&status=sold";
+                }
+            }
+        }
+        if(pending){
+            status += "pending";
+            if(sold){
+                status += "&status=sold";
+            }
+        }
+
+        return rs.get(status).then().log().all().extract().as(Pet[].class);
     }
 }
